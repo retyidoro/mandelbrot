66e5dbc691d6179548d17c29ba79216ac7ba83c7
Ferencz Istvan <ferencz.istvan996@gmail.com>
2024-09-12 16:18:14 +0300

    Some little change in comments

--- mandelbrot.js
+++ mandelbrot.js
@@ -10,22 +10,21 @@ let app = new Vue({
     maxit: 100,
     panX: 0.6,
     panY: 0.7,
-    ctx: ""
+    ctx: "",
   },
 
   //this happens when the page loads
+  //serios???
   mounted() {
-    console.log("mount")
+    console.log("mount");
     let canvas = document.getElementById("mandelbrot");
     canvas.width = this.width;
     canvas.height = this.height;
     this.ctx = canvas.getContext("2d");
-    //this.base()
-    this.drawMandelbrot()
+    this.drawMandelbrot();
   },
 
   methods: {
-
     go(direction) {
       if (direction === "left") {
         this.panX += 0.01;
@@ -36,8 +35,8 @@ let app = new Vue({
       } else if (direction === "down") {
         this.panY -= 0.01;
       }
-      
-      this.drawMandelbrot()
+
+      this.drawMandelbrot();
     },
 
     //https://progur.com/2017/02/create-mandelbrot-fractal-javascript.html
@@ -46,44 +45,49 @@ let app = new Vue({
       let imgCompRes = y;
       let maxIterations = this.maxit;
 
-      for (let i = 0 ; i < maxIterations; ++i) {
+      for (let i = 0; i < maxIterations; ++i) {
         //calculate real and img parts
-        let tempRealComp = realCompRes * realCompRes - imgCompRes * imgCompRes + x;
+        let tempRealComp =
+          realCompRes * realCompRes - imgCompRes * imgCompRes + x;
         let tempImgComp = 2 * realCompRes * imgCompRes + y;
 
         realCompRes = tempRealComp;
         imgCompRes = tempImgComp;
 
         if (realCompRes * imgCompRes > 5) {
-          return (i / maxIterations * 100);
+          return (i / maxIterations) * 100;
         }
-      }      
+      }
       return 0;
     },
     drawMandelbrot() {
       for (let x = 0; x < this.width; ++x) {
         for (let y = 0; y < this.height; ++y) {
-          let belongsToSet = this.checkIfMandelbrotPoint(x/this.magniFactor - this.panX, y/this.magniFactor - this.panY);
+          let belongsToSet = this.checkIfMandelbrotPoint(
+            x / this.magniFactor - this.panX,
+            y / this.magniFactor - this.panY
+          );
           //console.log(belongsToSet);
           if (belongsToSet == 0) {
-            this.ctx.fillStyle = '#000';
+            this.ctx.fillStyle = "#000";
             this.ctx.fillRect(x, y, 1, 1); // draw black pixel
-          }
-          else {
-            this.ctx.fillStyle = `hsl(${belongsToSet + parseInt(this.hue)}, 100%, ${ belongsToSet*this.lightness }%)`;
+          } else {
+            this.ctx.fillStyle = `hsl(${
+              belongsToSet + parseInt(this.hue)
+            }, 100%, ${belongsToSet * this.lightness}%)`;
             this.ctx.fillRect(x, y, 1, 1); //draw color pixel
           }
         }
       }
     },
     reset() {
-      this.magniFactor = 10000,
-      this.hue = 289,
-      this.lightness = 1,
-      this.panX = 0.6,
-      this.panY = 0.7
+      (this.magniFactor = 10000),
+        (this.hue = 289),
+        (this.lightness = 1),
+        (this.panX = 0.6),
+        (this.panY = 0.7);
 
-      this.drawMandelbrot()
-    }
-  }
-})
+      this.drawMandelbrot();
+    },
+  },
+});
